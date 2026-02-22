41e90ebb467d17399a697ba5af2665a8a25f9927
asavari-21 <asavarikaushal@gmail.com>
2026-02-22 12:26:23 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 def cmd_exit(args: any):
     sys.exit()
@@ -17,17 +18,23 @@ def cmd_type(args: str):
         print(f"{cmd} is a shell builtin")
         return
     
+    full_path = find_execute(cmd)
+    if full_path:
+        print(f"{cmd} is {full_path}")
+    else:       
+        print(f"{cmd}: not found")
+
+def find_execute(cmd):
     path_env = os.environ.get("PATH", "")
     paths = path_env.split(os.pathsep)
 
     for dir in paths:
         full_path = os.path.join(dir, cmd)
-
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-            print(f"{cmd} is {full_path}")
-            return
+            return full_path
         
-    print(f"{cmd}: not found")
+    return None
+
 
 builtin = {"echo": cmd_echo, "exit": cmd_exit, "type": cmd_type}
 
@@ -44,7 +51,14 @@ def main():
         if func:
             func(args)
         else:
-            print(f"{cmd}: command not found")
+            full_path = find_execute(cmd)
+            if full_path:
+                try:
+                    subprocess.run([full_path] + args)
+                except Exception as e:
+                    print(f"Error executing {cmd}: {e}")
+            else:
+                print(f"{cmd}: command not found")
 
 if __name__ == "__main__":
     main()
