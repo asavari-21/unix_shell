85ae9347f79a6fe31da2475ec6da76bb246de051
asavari-21 <asavarikaushal@gmail.com>
2026-02-22 11:39:27 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,6 +13,13 @@ def main():
             for i in range (1, len(command.split())):
                 print(command.split()[i], end = " ")
             print()
+        elif command.split()[0] == "type":
+            if command.split()[1] in ["echo", "exit", "type"]:
+                print(f"{command.split()[1]} is a shell bulletin.")
+            else:
+                for i in range (1, len(command.split())):
+                    print(command.split()[i], end = " ")
+                print("command not found")                                      
         else:
             print(f"{command}: command not found") 
 
