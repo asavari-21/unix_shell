6139b7cd5e4197893f1a2935b0c7e2a87051df49
asavari-21 <asavarikaushal@gmail.com>
2026-02-22 12:17:02 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,50 @@
+import os
 import sys
 
+def cmd_exit(args: any):
+    sys.exit()
+
+def cmd_echo(args: str):
+    print(" ".join(args))
+
+def cmd_type(args: str):
+    if not args:
+        print("type: missing argument")
+        return
+    
+    cmd = args[0]
+    if cmd in builtin:
+        print(f"{cmd} is a shell builtin")
+        return
+    
+    path_env = os.environ.get("PATH", "")
+    paths = path_env.split(os.pathsep)
+
+    for dir in paths:
+        full_path = os.path.join(dir, cmd)
+
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            print(f"{cmd} is {full_path}")
+            return
+        
+    print(f"{cmd}: not found")
+
+builtin = {"echo": cmd_echo, "exit": cmd_exit, "type": cmd_type}
 
 def main():
-    # sys.stdout.write("$ ")
-    # pass
     while True:
-        sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
-            sys.exit()
-        elif command.split()[0] == "echo":
-            for i in range (1, len(command.split())):
-                print(command.split()[i], end = " ")
-            print()
-        elif command.split()[0] == "type":
-            if command.split()[1] in ["echo", "exit", "type"]:
-                print(f"{command.split()[1]} is a shell builtin")
-            else:
-                print(f"{command.split()[1]}: not found")                                      
-        else:
-            print(f"{command}: command not found") 
+        cmd_input = input("$ ").strip()
+        if not cmd_input:
+            continue
 
+        cmd = cmd_input.split()[0]
+        args = cmd_input.split()[1:]
+
+        func = builtin.get(cmd)
+        if func:
+            func(args)
+        else:
+            print(f"{cmd}: command not found")
 
 if __name__ == "__main__":
     main()
