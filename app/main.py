e726902438241922ccbec7daf484b274e4e6c5ca
asavari-21 <asavarikaushal@gmail.com>
2026-03-14 19:39:43 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ def auto_complete(text, state):
         matches = sorted(cmd for cmd in cmds if cmd.startswith(text))
 
         if state < len(matches):
-            return matches[state]    
+            return matches[state] + " " 
         return None
     
     if "/" in text:
@@ -91,7 +91,7 @@ def auto_complete(text, state):
         search_dir = "."
 
     try:
-        entries = os.listir(search_dir)
+        entries = os.listdir(search_dir)
     except FileNotFoundError:
         return None
     
